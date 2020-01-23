77e9ce69ea87f67153fd5f0b51f4fa1ab214afc1
Rick Belson <belsonrj@aol.com>
2020-01-23 01:22:23 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,6 @@
 # lib/introduction.rb
 
-def introduction(name)
-  puts "Hi, my name is #{name}."
+def introduction("name", "language")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
